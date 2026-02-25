93324042c9ca113cf5934fd9b115576c0809976b
chiscookeke11 <chiscookeke11@gmail.com>
2026-02-25 04:52:07 +0100

    fixes

--- frontend/app/dashboard/maintenance/page.tsx
+++ frontend/app/dashboard/maintenance/page.tsx
@@ -1,5 +1,12 @@
 import MaintenanceFlow from '@/components/maintenance/MaintenanceFlow';
+import MaintenanceRequestForm from '@/components/maintenance/MaintenanceRequestForm';
 
 export default function TenantMaintenancePage() {
-  return <MaintenanceFlow defaultRole="tenant" />;
+    return (
+        <div className='w-full flex flex-col gap-16 items-center justify-center ' >
+            <MaintenanceRequestForm  />
+
+            <MaintenanceFlow defaultRole="tenant" />
+        </div>
+    );
 }
