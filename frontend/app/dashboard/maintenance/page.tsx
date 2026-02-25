cb08dad6261094022da7ada48f6a76f1c59873c9
chiscookeke11 <chiscookeke11@gmail.com>
2026-02-25 06:25:27 +0100

    fixes

--- frontend/app/dashboard/maintenance/page.tsx
+++ frontend/app/dashboard/maintenance/page.tsx
@@ -2,9 +2,5 @@ import MaintenanceFlow from '@/components/maintenance/MaintenanceFlow';
 import MaintenanceRequestForm from '@/components/maintenance/MaintenanceRequestForm';
 
 export default function TenantMaintenancePage() {
-    return (
-
-            <MaintenanceFlow defaultRole="tenant" />
-
-    );
+  return <MaintenanceFlow defaultRole="tenant" />;
 }
