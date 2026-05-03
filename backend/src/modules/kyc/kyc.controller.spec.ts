898d697321c071adbc47622fb7fb3b3859f7894d
technimite <dev@navin.local>
2026-05-03 20:04:14 +0100

    test: update agreements and kyc test specifications

--- backend/src/modules/kyc/kyc.controller.spec.ts
+++ backend/src/modules/kyc/kyc.controller.spec.ts
@@ -126,7 +126,7 @@ describe('KycController', () => {
       const res = await request(app.getHttpServer())
         .get('/kyc/status')
         .expect(200);
-      expect(res.body).toBeFalsy();
+      expect(res.body).toEqual({});
     });
   });
 
